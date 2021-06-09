1c8c05d0e91b253b44d7f4bd4113827f0db78df2
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-09 13:57:57 -0400

    SignUp, Login, LogOut all work correctly as should

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,21 @@ class SessionsController < ApplicationController
     def welcome
     end
 
-    def destory
+    def new
+    end
+
+    def create
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else 
+            flash[:error] = "Login unsucessful."
+            redirect_to login_path
+        end
+    end
+
+    def destroy
         session.delete(:user_id)
         redirect_to '/'
     end
