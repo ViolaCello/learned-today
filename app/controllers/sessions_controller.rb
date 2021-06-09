b6e83ab542e57f807cb967e90f9d1f0b4c778007
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-09 13:32:08 -0400

    Destroy session (ie., LogOut) in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,9 @@ class SessionsController < ApplicationController
     def welcome
     end
 
+    def destory
+        session.delete(:user_id)
+        redirect_to '/'
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_many :topics
     has_many :topics, through: :entry
+
+    validates :name, presence: true
+    validates :email, uniqueness: true, presence: true
     has_secure_password
-    
+
 end
