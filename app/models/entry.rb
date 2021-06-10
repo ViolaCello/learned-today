c2ccf31e9e8284b67b7d0eede3c980ae2d618b24
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-10 17:42:32 -0400

    I see that upon rendering, it doesn't respect the line breaks...

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,18 @@
 class Entry < ApplicationRecord
   belongs_to :user
   has_many :topics
+
+
+
+def possible_topics
+ 
+  text = self.post 
+  arr = text.split(' ')
+  # byebug
+  return arr
+end
+
+
+
+
 end
