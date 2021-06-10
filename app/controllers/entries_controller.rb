2fce75e87945a61447c8515333a0e1fceaa669c3
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-10 17:28:17 -0400

    Show page loads correctly

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,44 +1,52 @@
 class EntriesController < ApplicationController
-    before_action :require_login
+    # before_action :require_login
+ 
+     def index
+         @user = User.find_by_id(session[:user_id])
+         if @user 
+             @entries = @user.entries
+             redirect_to entries_path(@entries)
+         else 
+             byebug
+            # raise "Line 9 in Entry".inspect
+         end
+ 
+     end
+ 
+     def new
+         @entry = Entry.new
+     end
+ 
+     def create
+         @user = current_user if logged_in?
+         if @user
+             @entry = @user.entries.build(entry_params)
+             if @entry.save 
+                 #raise.params.inspect
+               #   byebug
+                 redirect_to entry_path(@entry)
+             else 
+                 @errors = @entry.errors
+                 render :'entries/new'
+             end
+         end
+     end
+ 
+     def show
+        @entry = Entry.find_by_id(params[:id])
+     end
 
-    def index
-        @user = User.find_by_id(params[:user_id])
-        if @user 
-            @entries = @user.entries
-        else 
-            raise "problem in EntriesController line 9".inspect
-        end
-
-    end
-
-    def new
-        @entry = Entry.new
-    end
-
-    def create
-        @user = current_user if logged_in?
-        if @user
-            @entry = @user.entries.build(entry_params)
-            if @entry.save 
-                raise.params.inspect
-                redirect_to entry_path(@entry)
-            else 
-                @errors = @entry.errors
-                render :'entries/new'
-            end
-        end
-    end
-
-    private
-
-    def require_login
-        return head(:forbidden) unless session.include? :user_id
-    end
-
-    private
-
-def entry_params
-    params.require(:entry).permit(:title, :post, :user_id)
-end
-
-end
+     private
+ 
+     def require_login
+         return head(:forbidden) unless session.include? :user_id
+     end
+ 
+     private
+ 
+ def entry_params
+     params.require(:entry).permit(:title, :post, :user_id)
+ end
+ 
+ end
+ 
