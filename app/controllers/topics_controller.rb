d0cfec1305df8941fdb16ffa0009a0dca4331a6f
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-16 14:13:35 -0400

    starting TopicsController

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,23 @@
 class TopicsController < ApplicationController
+
+    def new
+        @topic = Topic.new
+        @entry = Entry.find_by_id(params[:entry_id])
+        byebug
+    end
+
+    def create
+        raise.params.inspect
+        @user = current_user if logged_in?
+        @entry = Entry.find_by_id(params[:entry_id])
+    end
+
+
+
+
+    private 
+
+    def topic_params
+        params.require(:topic).permit(:keyword, :entry_id, :user_id)
+    end
 end
